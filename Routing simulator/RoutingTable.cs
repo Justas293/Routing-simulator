ae60af3a56d01936db767413cd778328982d1dc0
Justas <justasrasl@yahoo.com>
2017-04-24 17:15:41 +0300

    Add RoutingTable class

---
+++ Routing simulator/RoutingTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Routing_simulator
+{
+    public class RoutingTable
+    {
+        private List<string> DestinationIP { get; set; }
+        private List<string> NextHopIP { get; set; }
+        private List<int> DistanceMetric { get; set; }
+
+        public RoutingTable()
+        {
+
+        }
+
+    }
+}
